c09de2508fcaf2a7c24715270917c7ee2becfe39
rgarcia <voodoorai2000@gmail.com>
2011-05-11 15:15:44 +0200

    users scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110509130845) do
+ActiveRecord::Schema.define(:version => 20110511131527) do
 
   create_table "contacts", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(:version => 20110509130845) do
     t.boolean  "newsletter"
     t.string   "sex"
     t.text     "notes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
